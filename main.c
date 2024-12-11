8d684a5caf24b5be04a2c427105faae5907528a7
Growth Programmer <growthprogrammer@protonmail.com>
2024-12-10 23:55:18 -0500

    12/10/2024 Commit For Consistency

--- main.c
+++ main.c
@@ -6,13 +6,5 @@ int main(int argc, char **argv) {
         puts("EZ GameBoy Emulator Start Up Failed...");
         return EXIT_FAILURE;
     }
-    struct instruction {
-        int k;
-        int j;
-        char i;
-    };
-
-    struct instruction set = {};
-
     return EXIT_SUCCESS;
 }
