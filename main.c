56a5fc3800512387d6cfc451501a4ce8e93e249a
Growth Programmer <growthprogrammer@protonmail.com>
2024-12-11 23:18:10 -0500

    12/10/2024 Commit For Consistency

--- main.c
+++ main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+
+ /*
+ Author: Growth Programmer
+ Purpose: Game Boy Advanced Emulator Main
+ Date: 12/11/2024
+ */
 
 int main(int argc, char **argv) {
     if (puts("Starting EZ GameBoy Emulator...") == EOF) {
         puts("EZ GameBoy Emulator Start Up Failed...");
         return EXIT_FAILURE;
     }
+
+    printf("Enter a number:");
+    char buf[1024];
+    if (!fgets(buf, 300, stdin)){
+        return EXIT_FAILURE;
+    }
+    char *end_ptr;
+    errno = 0;
+    long a = strtol(buf, &end_ptr, 10);
+    printf("You entered %ld.\n", a);
+
+
     return EXIT_SUCCESS;
 }
